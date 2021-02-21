5ff6220520b0c6dc974551cbc962aa592921db78
Ivan <aiven715@gmail.com>
2021-02-21 16:53:52 +0300

    Redux

--- redux/src/lib.rs
+++ redux/src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+type Subscription<State> = fn(&State);
+type Reducer<State, Action> = fn(&State, &Action) -> State;
+
+pub struct Store<State, Action> {
+    state: State,
+    reducer: Reducer<State, Action>,
+    subscriptions: Vec<Subscription<State>>,
+}
+
+impl<State, Action> Store<State, Action> {
+    pub fn new(reducer: Reducer<State, Action>, initial_state: State) -> Self {
+        Store {
+            state: initial_state,
+            reducer,
+            subscriptions: vec![],
+        }
+    }
+
+    pub fn dispatch(&mut self, action: Action) {
+        let reducer = self.reducer;
+
+        self.state = reducer(&self.state, &action);
+
+        for sub in &self.subscriptions {
+            sub(&self.state);
+        }
+    }
+
+    pub fn get_state(&self) -> &State {
+        &self.state
+    }
+
+    pub fn subscribe(&mut self, sub: Subscription<State>) {
+        self.subscriptions.push(sub);
+    }
+
+    pub fn replace_reducer(&mut self, next_reducer: Reducer<State, Action>) {
+        self.reducer = next_reducer;
     }
 }
