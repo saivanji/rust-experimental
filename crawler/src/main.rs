eabca20ad4c9a8a2a477357723bbff2e417f33d2
Ivan <aiven715@gmail.com>
2021-02-23 02:51:50 +0300

    Fetching links of a given URL

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::task;
+use scraper::{Html, Selector};
+
+// TODO: get WEBSITE_URL from params
+const WEBSITE_URL: &str = "http://localhost:3001";
+
+fn main() -> surf::Result<()> {
+    task::block_on(fetch())
+}
+
+async fn fetch() -> surf::Result<()> {
+    let html = surf::get(WEBSITE_URL).await?.body_string().await?;
+    let document = Html::parse_document(&html);
+
+    // Why need to use "parse" for selectors instead of passing raw strings to "select"?
+    let links = Selector::parse("a").unwrap();
+
+    for link in document.select(&links) {
+        match link.value().attr("href") {
+            // TODO: needed link should start eigher with "/" or with "WEBSITE_URL"
+            Some(href) => println!("{}", href),
+            None => println!("No href"),
+        }
+    }
+
+    Ok(())
 }
