7b77e958220279f4633262cd540c986346af0d5a
Ivan <aiven715@gmail.com>
2021-03-01 01:49:20 +0300

    Fixes and starting implementing structs

--- crawler/src/page.rs
+++ crawler/src/page.rs
@@ -4,9 +4,41 @@ use scraper::{ElementRef, Html, Selector};
 use std::collections::BTreeSet;
 use std::fs;
 use std::io::Write;
+use std::iter;
 use std::path::{Path, PathBuf};
 use std::str;
 use url::Url;
+
+// TODO: rewrite using new structs.
+
+// split files by structs - url, page, node
+
+// struct Url {}
+
+// struct Page {}
+
+// impl Page {
+//     fn select() {}
+//     fn traverse() {}
+// }
+
+struct Node<'a> {
+    kind: NodeKind,
+    element: ElementRef<'a>,
+}
+
+impl<'a> Node<'a> {
+    fn new(kind: NodeKind, element: ElementRef<'a>) -> Self {
+        Self { kind, element }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum NodeKind {
+    Anchor,
+    Link,
+    Script,
+}
 
 #[async_recursion(?Send)]
 pub async fn process_page(url: &Url, workdir: &Path, trail: &mut BTreeSet<String>) -> Result<()> {
@@ -14,14 +46,34 @@ pub async fn process_page(url: &Url, workdir: &Path, trail: &mut BTreeSet<String
     let url_path = url.path();
 
     persist_file(&bytes, &url_path, &workdir)?;
-    trail.insert(url_path.to_string());
+    trail.insert(no_trailing_slash(url_path).to_string());
 
-    // TODO: do nothing if "bytes" could not be converted to utf8 or is not html string.
-    let text = str::from_utf8(&bytes).or(Err(anyhow!("Failed convert to string for {}", url)))?;
-    let page = Html::parse_document(text);
-    let links = Selector::parse("a").unwrap();
+    match convert_bytes(&bytes).and_then(parse_document) {
+        Some(page) => {
+            traverse(&page, url, workdir, trail).await?;
+            Ok(())
+        }
+        None => Ok(()),
+    }
+}
 
-    for link in page.select(&links) {
+async fn traverse(
+    page: &Html,
+    url: &Url,
+    workdir: &Path,
+    trail: &mut BTreeSet<String>,
+) -> Result<()> {
+    let mut all = Vec::new();
+
+    let mut anchors = select(page, NodeKind::Anchor);
+    let mut links = select(page, NodeKind::Link);
+    let mut scripts = select(page, NodeKind::Script);
+
+    all.append(&mut anchors);
+    all.append(&mut links);
+    all.append(&mut scripts);
+
+    for link in page.select(&anchors) {
         let should_process = |url: &Url| !trail.contains(no_trailing_slash(url.path()));
 
         match extract_url(&url, &link) {
@@ -33,9 +85,40 @@ pub async fn process_page(url: &Url, workdir: &Path, trail: &mut BTreeSet<String
     Ok(())
 }
 
+// TODO: will be moved to "Page" struct
+fn select(page: &Html, kind: NodeKind) -> Vec<Node> {
+    let selector = match kind {
+        NodeKind::Anchor => "a",
+        NodeKind::Link => "link",
+        NodeKind::Script => "script",
+    };
+
+    page.select(&Selector::parse(selector).unwrap())
+        .map(|element| Node::new(kind, element))
+        .collect()
+}
+
+fn convert_bytes(bytes: &Vec<u8>) -> Option<&str> {
+    match str::from_utf8(&bytes) {
+        Ok(text) => Some(&text),
+        Err(_) => None,
+    }
+}
+
+fn parse_document(text: &str) -> Option<Html> {
+    let page = Html::parse_document(text);
+
+    if page.errors.len() != 0 {
+        return None;
+    }
+
+    Some(page)
+}
+
 fn extract_url(url: &Url, link: &ElementRef) -> Option<Url> {
     link.value()
         .attr("href")
+        // filtering out external links
         .filter(|href| href.starts_with("/"))
         .and_then(|href| url.join(&href).ok())
 }
@@ -68,8 +151,12 @@ fn no_trailing_slash(input: &str) -> &str {
 fn split_path(path: &str) -> (&str, &str) {
     let default_filename = "index.html";
 
+    if path == "/" {
+        return ("", default_filename);
+    }
+
     match path.rsplit_once("/") {
-        Some((path, filename)) if filename.ends_with(".html") => (path, filename),
+        Some((path, filename)) if filename.contains(".") => (path, filename),
         _ => (path, default_filename),
     }
 }
