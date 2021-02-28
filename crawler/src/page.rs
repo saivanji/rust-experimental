bec0263f36bcbe0e6070498a8e653635bdd39eac
Ivan <aiven715@gmail.com>
2021-02-28 20:29:13 +0300

    Process multiple pages

--- crawler/src/page.rs
+++ crawler/src/page.rs
@@ -1,80 +1,87 @@
 use anyhow::{anyhow, Result};
+use async_recursion::async_recursion;
 use scraper::{ElementRef, Html, Selector};
 use std::collections::BTreeSet;
 use std::fs;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::str;
 use url::Url;
 
-const FETCH_ERROR: &str = "Url fetch error. Please make sure the url is correct";
-
+#[async_recursion(?Send)]
 pub async fn process_page(url: &Url, workdir: &Path, trail: &mut BTreeSet<String>) -> Result<()> {
-    let url_path = url.path().to_string();
+    let bytes = fetch(url).await?;
+    let url_path = url.path();
 
-    if trail.contains(&url_path) {
-        return Ok(());
-    }
+    persist_file(&bytes, &url_path, &workdir)?;
+    trail.insert(url_path.to_string());
 
-    let text = fetch_page(url).await?;
-    let page = Html::parse_document(&text);
+    // TODO: do nothing if "bytes" could not be converted to utf8 or is not html string.
+    let text = str::from_utf8(&bytes).or(Err(anyhow!("Failed convert to string for {}", url)))?;
+    let page = Html::parse_document(text);
     let links = Selector::parse("a").unwrap();
 
-    persist_page(&text, &url_path, &workdir)?;
-
-    trail.insert(url_path);
-
     for link in page.select(&links) {
-        process_link(&url, &link);
+        let should_process = |url: &Url| !trail.contains(no_trailing_slash(url.path()));
+
+        match extract_url(&url, &link) {
+            Some(url) if should_process(&url) => process_page(&url, workdir, trail).await?,
+            _ => continue,
+        }
     }
 
     Ok(())
 }
 
-fn process_link(url: &Url, link: &ElementRef) {
-    match link.value().attr("href") {
-        Some(href) => {
-            if href.starts_with("/") {
-                match url.join(&href) {
-                    Ok(link_url) => println!("{}", link_url),
-                    Err(_) => {}
-                }
-            }
-        }
-        None => {}
-    }
+fn extract_url(url: &Url, link: &ElementRef) -> Option<Url> {
+    link.value()
+        .attr("href")
+        .filter(|href| href.starts_with("/"))
+        .and_then(|href| url.join(&href).ok())
 }
 
-async fn fetch_page(url: &Url) -> Result<String> {
+async fn fetch(url: &Url) -> Result<Vec<u8>> {
     surf::get(url)
         .await
-        .or(Err(anyhow!(FETCH_ERROR)))?
-        .body_string()
+        .or(Err(anyhow!("Url fetch error. Url {} is not correct", url)))?
+        .body_bytes()
         .await
-        .or(Err(anyhow!(FETCH_ERROR)))
+        .or(Err(anyhow!("Failed to parse {} url", url)))
 }
 
-fn crop_slash(input: &str) -> &str {
-    match input.chars().nth(0) {
-        Some('/') => &input[1..],
-        _ => input,
+fn no_leading_slash(input: &str) -> &str {
+    if input == "/" {
+        return input;
     }
+
+    input.strip_prefix("/").unwrap_or(input)
+}
+
+fn no_trailing_slash(input: &str) -> &str {
+    if input == "/" {
+        return input;
+    }
+
+    input.strip_suffix("/").unwrap_or(input)
 }
 
 fn split_path(path: &str) -> (&str, &str) {
     let default_filename = "index.html";
 
-    let (scope, filename) = match path.rsplit_once("/") {
+    match path.rsplit_once("/") {
         Some((path, filename)) if filename.ends_with(".html") => (path, filename),
         _ => (path, default_filename),
-    };
-
-    (scope, crop_slash(filename))
+    }
 }
 
-fn persist_page(text: &str, url_path: &str, workdir: &Path) -> Result<()> {
+fn join(path: &PathBuf, part: &str) -> PathBuf {
+    path.join(no_leading_slash(part))
+}
+
+fn persist_file(bytes: &Vec<u8>, url_path: &str, workdir: &Path) -> Result<()> {
     let (scope, filename) = split_path(url_path);
-    let dir_path = workdir.join(scope);
-    let file_path = dir_path.join(filename);
+    let dir_path = join(&workdir.to_path_buf(), scope);
+    let file_path = join(&dir_path, filename);
 
     let file_display = file_path.display();
     let file_create_err = Err(anyhow!("Can not create file {}", file_display));
@@ -87,7 +94,7 @@ fn persist_page(text: &str, url_path: &str, workdir: &Path) -> Result<()> {
 
     fs::File::create(&file_path)
         .or(file_create_err)?
-        .write(text.as_bytes())
+        .write(bytes)
         .or(file_write_err)?;
 
     Ok(())
